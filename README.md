c0c50afee74f3080c23f07b2987b278044923549
Adrien PAYEN <adrienpayen@users.noreply.github.com>
2016-09-30 17:03:56 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Config zsh, git, php
+=========================
+
+![](https://raw.github.com/lesmyrmidons/dotfiles/master/img/screenshot.png)
+
+
+Intallation
+-----------
+
+Just run : `sudo ./install.sh [options]`
+
+Options
+-------
+
+Possibility not to install some component of the script thanks to options follows:
+
+* `--no-zsh`: not install zsh.
+* `--no-composer`: not install composer.
+* `--no-term`: not install term terminator.
+* `--help`: Display this help message.
---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+CURRENT=`pwd`
+INSTALL_COMPOSER=true
+INSTALL_ZSH=true
+
+# Parsing options
+if [ $# -gt 0 ] ; then
+    for arg in $@ ; do
+        if [ $arg = '--no-zsh' ] ; then
+            INSTALL_ZSH=false
+        elif [ $arg = '--no-composer' ] ; then
+            INSTALL_COMPOSER=false
+        elif [ $arg = '--help' ] ; then
+            printf "\033[0;32mDotfiles adrienpayen\033[0m\n\n"
+            printf "\033[0;33mUsage:\033[0m\n"
+            printf "  ./install.h [options]\n\n"
+            printf "\033[0;33mOptions:\033[0m\n"
+            printf "  \033[0;32m--no-zsh\033[0m\t\tDon't install zsh\n"
+            printf "  \033[0;32m--no-composer\033[0m\t\tDon't install composer\n"
+            printf "  \033[0;32m--help\033[0m\t\tDisplay this help message\n"
+            exit 0;
+        fi
+    done
+fi
+
+PACKAGE='git git-core curl'
+
+printf "Install package ------------------------- \033[0;32m$PACKAGE\033[0m\n"
+sudo brew update
+sudo brew install $PACKAGE
+echo ""
+printf "Test files exist ------------------------ \033[0;32m~/.gitconfig ~/.gitignore_global ~/.config/fontconfig\033[0m\n"
+if [ -f ~/.gitconfig ] ; then
+    cat ~/.gitconfig > ~/gitconfig.backup
+    rm -f ~/.gitconfig
+    printf "   \033[0;36mExisting .gitconfig\t\t> save\033[0m \033[0;32mgitconfig.backup\033[0m\n"
+fi
+
+if [ -f ~/.gitignore_global ] ; then
+    cat ~/.gitignore_global > ~/gitignore_global.backup
+    rm -f ~/.gitignore_global
+    printf "   \033[0;36mExisting .gitignore_global\t> save\033[0m \033[0;32mgitignore_global.backup\033[0m\n"
+fi
+
+if [ -f ~/.config/fontconfig ] ; then
+    rm -rf ~/.config/fontconfig
+    printf "   \033[0;36mExisting .config/fontconfig\033[0m \033[0;33m=> remove\033[0m\n"
+fi
+
+echo ""
+printf "Create symlinks:\n"
+printf "   \033[0;36m.gitconfig\033[0m\n"
+ln -sf $CURRENT/gitconfig ~/.gitconfig
+printf "   \033[0;36m.gitignore_global\033[0m\n"
+ln -sf $CURRENT/gitignore_global ~/.gitignore_global
+printf "   \033[0;36m.config/fontconfig\033[0m\n"
+ln -sf $CURRENT/_config/fontconfig/ ~/.config/fontconfig
+printf "   \033[0;36m.fonts\033[0m\n"
+ln -sf $CURRENT/_fonts/ ~/.fonts
+
+echo ""
+printf "Load fonts:\n"
+fc-cache -vf ~/.fonts
+echo ""
+
+if $INSTALL_COMPOSER ; then
+    echo "Installation de composer ------------------------- "
+    lib/composer.sh
+fi
+
+if $INSTALL_ZSH ; then
+    git submodule update --init
+    git submodule foreach git pull origin master
+    echo "Installation de zsh --------------------------- "
+    cd $CURRENT/zsh-config
+    ./install.sh
+fi
+echo "End"
